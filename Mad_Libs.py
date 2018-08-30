2768671f2bd04f2e22c47c6c366a492d399b109c
Mintri1199 <mintri1199@gmail.com>
2018-08-30 13:54:41 -0700

    Input changing ideas implemented

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -13,20 +13,39 @@ prompt_for_user = ['noun ', 'verb ']
 nounList = []
 
 
+def change_input(inputted):
+
+    for index, string in enumerate(inputted):
+        print(index, string)
+
+    flag = input("Do you want to change your answer?  Y/N")
+    while flag.upper() == "Y":
+        try:
+            changing = input('Input the number associated to the desired item: ')
+
+            inputted[int(changing)] = input("Please input a " +
+                                            prompt_for_user[int(changing)])
+            print(*inputted)
+
+        except TypeError:
+            exit()
+
+
+# Determine the amount of inputs require for the selected Mad Lib
 def required_inputs():
     input_taken = []
     if len(prompt_for_user) == testList[0].count('{}'):
         for i in prompt_for_user:
-            input_taken.append(user_input(i))
+            input_taken.append(input(i))
 
-    noun_insert(input_taken)
+    change_input(input_taken)
 
 
 def noun_input():
     i = 0
     while i < testList[0].count('{}'):
 
-        noun = user_input('Please input a noun: ')
+        noun = input('Please input a noun: ')
         nounList.append(noun)
         i += 1
 
@@ -34,14 +53,6 @@ def noun_input():
 def noun_insert(value):
 
     print(testList[0].format(*value))
-
-    # testList[0] = testList[0].format(i)
-    # print(testList[0].format( nounList[0], nounList[1]))
-
-
-def user_input(prompt):
-
-    return input(prompt)
 
 
 def test():
