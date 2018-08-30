822d89117251c5e00bb0197bb65c2896400ab3aa
Jackson Ho <mintri1199@gmail.com>
2018-08-30 11:20:01 -0700

    implement working string replacement

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -4,12 +4,47 @@ user_inputs = []
 
 # Mad_Libs = {'Mad Lib 1 title': "sososo"}
 
-#for title, stories in Mad_Libs.items():
-#    print(title)
+# The part of speech list are empty
 
 
+testList = ['This is {!r} and this is {!r}']
+
+nounList = []
+
+verbList = []
+
+adjectiveList = []
+
+
+def noun_input():
+    i = 0
+    while i < testList[0].count('{!r}'):
+
+        noun = user_input('Please input a noun: ')
+        nounList.append(noun)
+        i += 1
+
+
+def noun_insert():
+
+    print(testList[0].format( nounList[0], nounList[1]))
+
+
+def user_input(prompt):
+
+    return input(prompt)
 
 
 def test():
+    # The user will be able to input
     pass
 
+
+
+
+
+noun_input()
+
+print(nounList)
+
+noun_insert()
