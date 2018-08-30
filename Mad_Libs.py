3625ac2af8cf1c6aadb13238da2e626766b373cf
Jackson Ho <mintri1199@gmail.com>
2018-08-29 21:17:07 -0700

    Initial commit for Mad_Libs.py

---
+++ Mad_Libs.py
@@ -0,0 +1,15 @@
+print("Welcome to Mad Libs")
+
+user_inputs = []
+
+# Mad_Libs = {'Mad Lib 1 title': "sososo"}
+
+#for title, stories in Mad_Libs.items():
+#    print(title)
+
+
+
+
+def test():
+    pass
+
