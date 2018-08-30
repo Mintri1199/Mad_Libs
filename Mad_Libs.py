5156eaf84b8f34af11632d539e1b82802b066201
Jackson Ho <mintri1199@gmail.com>
2018-08-30 13:20:52 -0700

    Repeated user inputs, and insert function implemented

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -7,27 +7,36 @@ user_inputs = []
 # The part of speech list are empty
 
 
-testList = ['This is {!r} and this is {!r}']
+testList = ['This is {} and this is {}']
 
+prompt_for_user = ['noun ', 'verb ']
 nounList = []
 
-verbList = []
 
-adjectiveList = []
+def required_inputs():
+    input_taken = []
+    if len(prompt_for_user) == testList[0].count('{}'):
+        for i in prompt_for_user:
+            input_taken.append(user_input(i))
+
+    noun_insert(input_taken)
 
 
 def noun_input():
     i = 0
-    while i < testList[0].count('{!r}'):
+    while i < testList[0].count('{}'):
 
         noun = user_input('Please input a noun: ')
         nounList.append(noun)
         i += 1
 
 
-def noun_insert():
+def noun_insert(value):
 
-    print(testList[0].format( nounList[0], nounList[1]))
+    print(testList[0].format(*value))
+
+    # testList[0] = testList[0].format(i)
+    # print(testList[0].format( nounList[0], nounList[1]))
 
 
 def user_input(prompt):
@@ -40,11 +49,4 @@ def test():
     pass
 
 
-
-
-
-noun_input()
-
-print(nounList)
-
-noun_insert()
+required_inputs()
